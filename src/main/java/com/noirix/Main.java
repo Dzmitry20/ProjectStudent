48f43884d08d72e940dd7728225f8d5b04c8ab3b
Dzmitry <zhdv27@mail.ru>
2021-04-08 11:18:37 +0300

    cherry branch commit

--- src/main/java/com/noirix/Main.java
+++ src/main/java/com/noirix/Main.java
@@ -6,6 +6,7 @@ public class Main {
         System.out.println("World!!!!!!!!");
         System.out.println("!!!!!!!!!!!!!");
         System.out.println("feature");
+        System.out.println("Cherry");
 
     }
 }
